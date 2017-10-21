8a625e5a101ccee4470c302bd6cb2182feae3ef2
florwilyn <florcayson@gmail.com>
2017-10-21 10:39:07 +0800

    initial search loop

--- MazePathSearch.java
+++ MazePathSearch.java
@@ -7,6 +7,7 @@ class MazePathSearch{
 	public List<Square> closedList;
 	public List<Square> parentList;
 	Square goal;
+	Square start;
 
 	public MazePathSearch(){
 		openList = new ArrayList<Square>();
@@ -24,6 +25,8 @@ class MazePathSearch{
 				 Maze[i][j] = new Square(mazefile.content.get(i).charAt(j), j, i);
 				 if (Maze[i][j].state == '.')
 				 	goal = Maze[i][j];
+				 else if (Maze[i][j].state == 'P')
+				 	start = Maze[i][j];
 			}
 		}
 	}
@@ -37,27 +40,80 @@ class MazePathSearch{
 		}
 	}
 
-	public void findAdjacent (Square square){
+	public List<Square> findAdjacent (Square square){
+		List<Square> adjacent = new ArrayList<Square>();
 		for (int i = square.yPos - 1; i <= square.yPos + 1; i++){
 			for (int j = square.xPos - 1; j <= square.xPos + 1; j++){
 				if (!(i == square.yPos && j == square.xPos)){
 					if (i >= 0 && i < Maze.length && j >= 0 && j < Maze[0].length){
 						if (Maze[i][j].state == ' '){
 							Maze[i][j].parent = square;
-							openList.add(Maze[i][j]);
+							//openList.add(Maze[i][j]);
+							adjacent.add(Maze[i][j]);
 						}
 					}
 				}
 			}
 		}
+		return adjacent;
 	}
 
+	//for up, down, left right movements h
 	public int ManhattanDistance(Square square){
 		return Math.abs(square.xPos - goal.xPos) + Math.abs(square.yPos - goal.yPos);
 	} 
 
+	//for diagonal h
 	public int StraightLineDistance(Square square){
 		return Math.max(Math.abs(square.xPos - goal.xPos),Math.abs(square.yPos - goal.yPos));
+	}
+
+	public Square findMinf(){
+		Square min = openList.get(0);
+		for(Square square: openList){
+			if (min.f > square.f)
+				min = square;
+		}
+		return min;
+	}
+
+	public Square findMinG(){
+		Square min = openList.get(0);
+		for(Square square: openList){
+			if (min.g > square.g)
+				min = square;
+		}
+		return min;
+	}
+
+	public void search(){
+		openList.add(start);
+		while (openList.size() > 0){
+			Square q = findMinf();
+			openList.remove(q);
+			List<Square> adjacent = findAdjacent(q);
+			for (int i=0; i<adjacent.size(); i++){
+				Square successor = adjacent.get(i);
+				if (!openList.contains(successor)){
+					successor.g = q.g + 1;
+					if (!diagonal(successor)) //if successor is diagonal; code
+						successor.h = ManhattanDistance(successor);
+					else
+						successor.h = StraightLineDistance(successor);
+					successor.f = successor.g + successor.h;
+					openList.add(new Square(successor));
+				}
+				else{
+					
+				}
+			}
+		}
+
+	}
+
+	public boolean diagonal(Square square){
+		//if ()
+		return true;
 	}
 
 
@@ -66,6 +122,8 @@ class MazePathSearch{
 		MazePathSearch search = new MazePathSearch();
 		search.Load_Maze();
 		search.Display_Maze();
+
+
 	}
 }
 
@@ -120,5 +178,13 @@ class Square{
 			start = false;
 			goal = true;
 		}
+	}
+
+	public Square(Square square){
+		xPos = square.xPos;
+		yPos = square.yPos;
+		g = square.g;
+		h = square.h;
+		f = square.f;
 	}
 }
